ebc1ca3f64c3323a91ec99d9d8f8ae3f44583d78
Abdelaziz Bardich <abdelazizbardich@gmail.com>
2024-12-30 21:38:41 +0100

    Add deploy command console and link it to route as a web hook

--- routes/console.php
+++ routes/console.php
@@ -6,3 +6,22 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+
+Artisan::command('deploy', function () {
+    // reset all changes
+    $output = shell_exec('git reset --hard');
+    $output = shell_exec('git pull origin main');
+    $output = shell_exec('composer install');
+    $output = shell_exec('php artisan migrate');
+    $output = shell_exec('php artisan config:cache');
+    $output = shell_exec('php artisan route:cache');
+    $output = shell_exec('php artisan view:cache');
+    // $output = shell_exec('php artisan queue:restart');
+    $output = shell_exec('php artisan optimize');
+    // move .htaccess file to public folder
+    $output = shell_exec('cp ../deploy-files/.htaccess ./.htaccess');
+    // move .env file to root folder
+    $output = shell_exec('cp ../deploy-files/.env.production ./.env');
+    $this->comment($output);
+})->purpose('Deploy the latest changes from GitHub');
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/deploy', function () {
+    Artisan::call('deploy');
+    // save the output from the command in a log file
+    $output = Artisan::output();
+    file_put_contents(storage_path('logs/deploy.log'), $output);
 });
