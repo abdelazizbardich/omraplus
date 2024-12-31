00a49f854bd8f8e44432bd6c271e9dd44886ffa4
Abdelaziz Bardich <abdelazizbardich@gmail.com>
2024-12-31 17:47:37 +0100

    add hearo header overlay

--- resources/views/components/hero-header.blade.php
+++ resources/views/components/hero-header.blade.php
@@ -2,10 +2,11 @@
 w-full 
 bg-blue-1000 
 hero-header
-back
+bg-cover
+bg-center
 " style="background-image: url({{ Vite::asset('resources/images/hero-header.webp') }});">
-    <div class="container h-full mx-auto flex items-start justify-center flex-col">
-        <h1 class="text-white text-5xl leading-relaxed mb-5 font-bold">{{ __('hero header title') }} <br> {{ __('hero header title 2') }}</h1>
+    <div class="relative z-10 container h-full mx-auto flex items-start justify-center flex-col">
+        <h1 class="drop-shadow-md text-white text-5xl leading-relaxed mb-5 font-bold">{{ __('hero header title') }} <br> {{ __('hero header title 2') }}</h1>
             <p class="text-white text-2xl leading-relaxed">نحن نلتزم بتقديم خدمات شاملة ومميزة لضيوف الرحمن، حيث نجعل من رحلات العمرة والحج تجربة سهلة ومريحة بفضل خدماتنا المتكاملة التي تشمل حجز التذاكر، الإقامة، والنقل. نحرص على تلبية جميع احتياجاتكم بروح من المصداقية والاحترافية، لتعيشوا أجواء الإيمان والسكينة بكل طمأنينة.</p>
     </div>
 </div>
