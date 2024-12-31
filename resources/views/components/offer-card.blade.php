555d606ada078e28e4a6140452df82ca992ecf92
Abdelaziz Bardich <abdelazizbardich@gmail.com>
2024-12-31 22:42:28 +0100

    build

---
+++ resources/views/components/offer-card.blade.php
@@ -0,0 +1,6 @@
+<div class="border rounded-3xl w-full h-full m-3 overflow-hidden">
+    <div class="w-full">
+        <img src="{{ Vite::asset('resources/images/umrah.jpg') }}" class="w-full h-full mb-3" alt="umrah">
+    </div>
+    <h1></h1>
+</div>
