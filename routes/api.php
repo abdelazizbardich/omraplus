e5dd90d9dcd3fea6000052ac286d932c8fa3e35d
Abdelaziz Bardich <abdelazizbardich@gmail.com>
2025-01-01 23:50:29 +0100

    Make deploy logs versioned

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,6 @@ Route::post('/deploy', function () {
     Artisan::call('deploy');
     // save the output from the command in a log file
     $output = Artisan::output();
-    file_put_contents(storage_path('logs/deploy.log'), $output);
-
+    file_put_contents(storage_path('logs/deploy-' . date('Y-m-d') . '.log'), $output);
     return response(true);
 });
