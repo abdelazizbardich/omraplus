2fcc0cc65c7d6059e7c3c7166b2a5e79dc18aecd
Abdelaziz Bardich <abdelazizbardich@gmail.com>
2025-01-01 02:21:35 +0100

    Add Offer card with preview and fix translation

---
+++ resources/views/components/offer-card-with-preview.blade.php
@@ -0,0 +1,64 @@
+<div class="relative w-full h-full mt-8">
+    <span class="bg-black-900 rounded-full py-1 px-10 text-white text-lg font-semibold absolute -top-8 start-0 block z-10">{{__('Best Seller')}}</span>
+    <div class="border border-slate-600 rounded-3xl w-full h-full mt-3 overflow-hidden relative">
+        <div class="w-full relative mb-8">
+            <div class="w-full h-80 bg-black-300">
+                <img src="{{ Vite::asset('resources/images/umrah2.jpg') }}" class="w-full h-full object-cover"  alt="umrah">
+            </div>
+            <span class="pointer bg-white w-fit py-2 px-3 border rounded-md block text-xl text-center font-bold flex items-center justify-center flex-row absolute m-auto start-0 end-0 -bottom-6">
+                <em class="fa-regular fa-eye me-3"></em>{{__('Quick view')}}
+            </span>
+        </div>
+        <div class="p-3">
+            <div class="flex">
+                <span class="bg-black-900 w-20 h-16 me-3 rounded-md text-white block text-base text-center font-bold flex items-center justify-center flex-col leading-5"><span>7</span><span>{{__('Days')}}</span></span>
+                <h1 class="font-black text-lg w-full rounded-full w-fit m-auto">عمرة شعبان - من 19 فبراير إلى 06 مارس - عبر الخطوط السعودية</h1>
+            </div>
+            <hr class="my-3">
+            <div class="flex justify-between items-stretch mb-2">
+                <div class="bg-orange-300 h-16 w-16 rounded overflow-hidden">
+                    <img src="" class="w-full h-full object-cover aspect-square">
+                </div>
+                <div class="bg-orange-300 h-16 w-16 rounded overflow-hidden">
+                    <img src="" class="w-full h-full object-cover aspect-square">
+                </div>
+                <div class="bg-orange-300 h-16 w-16 rounded overflow-hidden">
+                    <img src="" class="w-full h-full object-cover aspect-square">
+                </div>
+                <div class="bg-orange-300 h-16 w-16 rounded overflow-hidden">
+                    <img src="" class="w-full h-full object-cover aspect-square">
+                </div>
+                <div class="bg-orange-300 h-16 w-16 rounded overflow-hidden">
+                    <img src="" class="w-full h-full object-cover aspect-square">
+                </div>
+            </div>
+            <span class="font-black text-lg w-full rounded-full w-fit m-auto underline-offset-8 underline mb-3 block">{{ __('Upcoming flights') }}</span>
+            <div class="mb-3">
+                <div class="flex items-end justify-between w-full text-sm mb-2">
+                    <span class="font-normal">12 نوفمبر</span>
+                    <span class="dots flex-1 border-b-2 mb-1 mx-3 border-dotted border-blue-1000 h-full"></span>
+                    <span class="font-normal">{{__('with')}} الخطوط الملكية المغربية</span>
+                </div>
+                <div class="flex items-end justify-between w-full text-sm mb-2">
+                    <span class="font-normal">13 نوفمبر</span>
+                    <span class="dots flex-1 border-b-2 mb-1 mx-3 border-dotted border-blue-1000 h-full"></span>
+                    <span class="font-normal">{{__('with')}} الخطوط الملكية المغربية</span>
+                </div>
+                <div class="flex items-end justify-between w-full text-sm mb-2">
+                    <span class="font-normal">14 نوفمبر</span>
+                    <span class="dots flex-1 border-b-2 mb-1 mx-3 border-dotted border-blue-1000 h-full"></span>
+                    <span class="font-normal">{{__('with')}} الخطوط الملكية المغربية</span>
+                </div>
+            </div>
+            <div class="flex justify-between items-center mb-2">
+                <div class="flex flex-col">
+                    <span class="text-lg font-black">14.000,00 {{__('dh')}} {{__('For a person')}}</span>
+                    <span class="text-xs text-nowrap">({{__('Price varies depending on the size of the collection')}})</span>
+                </div>
+                <div class=" bg-yellow-500 shadow w-fit text-nowrap shadow-yellow-700 rounded-md h-full py-2 px-4 text-xl font-black group duration-300 transition-colors cursor-pointer">
+                    <em class="fa-solid fa-chevron-left me-2 group-hover:me-1 group-hover:ps-1 duration-300"></em> {{__('Book now')}}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- resources/views/components/offer-card.blade.php
+++ resources/views/components/offer-card.blade.php
@@ -1,9 +1,9 @@
-<div class="relative w-full h-full mb-12">
-    <span class="bg-black-900 rounded-full py-1 px-10 text-white text-lg font-semibold absolute -top-8 start-0 block z-10">Best Seller</span>
-    <div class="border border-slate-600 rounded-3xl w-full h-full my-3 overflow-hidden relative">
+<div class="relative w-full h-full mt-8">
+    <span class="bg-black-900 rounded-full py-1 px-10 text-white text-lg font-semibold absolute -top-8 start-0 block z-10">{{__('Best Seller')}}</span>
+    <div class="border border-slate-600 rounded-3xl w-full h-full mt-3 overflow-hidden relative">
         <div class="w-full relative">
             <img src="{{ Vite::asset('resources/images/umrah.jpg') }}" class="w-full h-80" alt="umrah">
-            <span class="bg-yellow-500 w-20 h-20 rounded-md text-white block text-xl text-center font-bold flex items-center justify-center flex-col leading-6 absolute start-4 bottom-4"><span>7</span><span>Days</span></span>
+            <span class="bg-yellow-500 w-20 h-20 rounded-md text-white block text-xl text-center font-bold flex items-center justify-center flex-col leading-6 absolute start-4 bottom-4"><span>7</span><span>{{__('Days')}}</span></span>
         </div>
         <div class="p-3">
             <h1 class="font-black text-xl w-full rounded-full w-fit m-auto">عمرة شعبان - من 19 فبراير إلى 06 مارس - عبر
@@ -12,34 +12,34 @@
             <p class="text-sm mb-3">
                 {{__('Lorem ipsum')}}
             </p>
-            <span class="font-black text-lg w-full rounded-full w-fit m-auto">الرحلات القادمة</span>
+            <span class="font-black text-lg w-full rounded-full w-fit m-auto mb-2 block">{{ __('Upcoming flights') }}</span>
             <div class="mb-3">
-                <div class="flex items-end justify-between w-full text-sm">
+                <div class="flex items-end justify-between w-full text-sm mb-1">
                     <span class="font-semibold">12 نوفمبر</span>
                     <span class="dots flex-1 border-b-2 mb-1 mx-3 border-dotted border-blue-1000 h-full"></span>
-                    <span class="font-semibold">مع الخطوط الملكية المغربية</span>
+                    <span class="font-semibold">{{__('with')}} الخطوط الملكية المغربية</span>
                 </div>
-                <div class="flex items-end justify-between w-full text-sm">
+                <div class="flex items-end justify-between w-full text-sm mb-1">
                     <span class="font-semibold">13 نوفمبر</span>
                     <span class="dots flex-1 border-b-2 mb-1 mx-3 border-dotted border-blue-1000 h-full"></span>
-                    <span class="font-semibold">مع الخطوط الملكية المغربية</span>
+                    <span class="font-semibold">{{__('with')}} الخطوط الملكية المغربية</span>
                 </div>
-                <div class="flex items-end justify-between w-full text-sm">
+                <div class="flex items-end justify-between w-full text-sm mb-1">
                     <span class="font-semibold">14 نوفمبر</span>
                     <span class="dots flex-1 border-b-2 mb-1 mx-3 border-dotted border-blue-1000 h-full"></span>
-                    <span class="font-semibold">مع الخطوط الملكية المغربية</span>
+                    <span class="font-semibold">{{__('with')}} الخطوط الملكية المغربية</span>
                 </div>
             </div>
             <div class="flex flex-col mb-3">
-                <span class="text-2xl font-black">14.000,00 للشخص</span>
-                <span class="text-xl font-black">(يختلف السعر حسب حجم المجموعة)</span>
+                <span class="text-2xl font-black">14.000,00 {{__('dh')}} {{__('For a person')}}</span>
+                <span class="text-xl font-black">({{__('Price varies depending on the size of the collection')}})</span>
             </div>
             <div class="rounded-full bg-black-900 flex justify-between items-stretch w-full ms-1">
                 <div class="bg-emerald-550 text-white rounded-full px-4 p-1 -ms-1 flex items-center hover:bg-emerald-600 hover:shadow-lg cursor-pointer duration-300 transition-colors">
-                    <em class="fa-brands fa-whatsapp text-3xl me-3"></em> الحجز السريع
+                    <em class="fa-brands fa-whatsapp text-3xl me-3"></em> {{__('The Fast booking')}}
                 </div>
                 <div class=" text-white rounded-full h-full py-2 px-5 text-xl group duration-300 transition-colors cursor-pointer">
-                    <em class="fa-solid fa-chevron-left me-2 group-hover:me-1 duration-300"></em> إحجـز الآن
+                    <em class="fa-solid fa-chevron-left me-2 group-hover:me-1 duration-300"></em> {{__('Book now')}}
                 </div>
             </div>
         </div>
