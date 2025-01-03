fdd8da81edb850dd977630ab6798d38eb7885a69
Abdelaziz Bardich <abdelazizbardich@gmail.com>
2025-01-03 18:42:02 +0100

    Revert "Offer page working on offer card with prev (minimal)"
    This reverts commit 9fc180c3a88d20a58105faa2c20b40fdfc51aaa8.

--- resources/views/components/atoms/title.blade.php
+++ resources/views/components/atoms/title.blade.php
@@ -1,4 +1,4 @@
 <div class="w-8/12 m-auto px-10 flex flex-col">
     <h2 class="font-black fon text-3xl w-full px-10 py-3 rounded-full w-fit m-auto text-center">{{$title}}</h2>
-    @if($subtitle)<p class="font-medium text-xl py-3 px-4 w-fit m-auto text-center leading-relaxed">{{$subtitle}}</p>@endif
+    <p class="font-medium text-xl py-3 px-4 w-fit m-auto text-center leading-relaxed">{{$subtitle}}</p>
 </div>
