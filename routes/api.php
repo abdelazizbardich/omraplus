749eb1556ec17b913bddf05add6aba4d68d49f91
Abdelaziz Bardich <abdelazizbardich@gmail.com>
2025-01-04 13:40:21 +0100

    Fix console

--- routes/api.php
+++ routes/api.php
@@ -12,7 +12,7 @@ Route::get('/user', function (Request $request) {
 Route::post('/deploy', function () {
     Artisan::call('deploy');
     // save the output from the command in a log file
-    $output = Artisan::output();
-    file_put_contents(storage_path('logs/deploy-' . date('Y-m-d') . '.log'), $output);
+    // $output = Artisan::output();
+    // file_put_contents(storage_path('logs/deploy-' . date('Y-m-d') . '.log'), $output);
     return response(true);
 });
