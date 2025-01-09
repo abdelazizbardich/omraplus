aff10ee2af2eb58d2c8c524f18ddb0ac5c76e665
Abdelaziz Bardich <abdelazizbardich@gmail.com>
2025-01-09 12:41:57 +0100

    Work on mobile home page

--- resources/views/mobile/pages/home.blade.php
+++ resources/views/mobile/pages/home.blade.php
@@ -1,16 +1,11 @@
 @extends('layouts.mobile')
 @section('content')
-<div class="bg-blue-950 w-full h-dvh flex items-center justify-center">
-    <div class="container mx-auto">
-        <div class="flex flex-col w-full items-center justify-center">
-            <a href="{{ route('home') }}" class="block mb-8 text-center">
-                <img src="{{ Vite::asset('resources/images/logo.webp') }}" alt="{{ __('Logo') }}" class="h-16 w-full">
-            </a>
-            <span class="text-white text-lg font-base block text-center mb-6">{{__('Mobile version is loading')}}...</span>
-            <div class="">
-                <span class="loading loading-dots loading-lg text-white"></span>
-            </div>
-        </div>
+<x-mobile.header></x-mobile.header>
+<x-mobile.hero-header></x-mobile.hero-header>
+<!-- <div class="px-6 flex gap-3 py-12">
+    <div class="w-6/12">
+    <x-mobile.offer-card-minimal-with-preview></x-mobile.offer-card-minimal-with-preview>
     </div>
-</div>
+    <div class="w-6/12"></div>
+</div> -->
 @endsection
