821f93fb7c2cd864fc23f3c479cc18c8182bfcd8
Abdelaziz Bardich <abdelazizbardich@gmail.com>
2025-01-09 12:45:53 +0100

    Update home.blade.php

--- resources/views/mobile/pages/home.blade.php
+++ resources/views/mobile/pages/home.blade.php
@@ -2,10 +2,4 @@
 @section('content')
 <x-mobile.header></x-mobile.header>
 <x-mobile.hero-header></x-mobile.hero-header>
-<!-- <div class="px-6 flex gap-3 py-12">
-    <div class="w-6/12">
-    <x-mobile.offer-card-minimal-with-preview></x-mobile.offer-card-minimal-with-preview>
-    </div>
-    <div class="w-6/12"></div>
-</div> -->
 @endsection
