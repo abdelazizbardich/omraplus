f580ee8ccb6fce3f47dd4ae7ed108fc9e945448a
Abdelaziz Bardich <abdelazizbardich@gmail.com>
2025-03-16 09:05:48 +0000

    Update FligthController.php

--- app/Http/Controllers/FligthController.php
+++ app/Http/Controllers/FligthController.php
@@ -95,6 +95,68 @@ class FligthController extends Controller
         return view('flights', $data);
     }
 
+    public function update(Request $request, Flight $flight)
+    {
+        $request->validate([
+            "title" => "required",
+            "going_date" => "required",
+            "return_date" => "required",
+            "type" => "required",
+            "category" => "required",
+            "aireline" => "required",
+            "is_recommended" => "required",
+            "description" => "required",
+        ]);
+
+        
+        // Save data to database
+        $flight->update([
+            "title" => $request->title,
+            "slug" => STR::slug($request->title),
+            "going_date" => $request->going_date,
+            "return_date" => $request->return_date,
+            "type" => $request->type,
+            "category_id" => $request->category,
+            "airline_id" => $request->aireline,
+            "is_recommended" => $request->is_recommended === "on",
+            "description" => $request->description
+        ]);
+        
+        if($request->hasFile('primary_photo')){
+            // Validate file primary_photo and hotel_photos
+            $request->validate([
+                'primary_photo' => 'required|mimes:jpeg,png,jpg,gif,svg,webp|max:2048',
+            ]);
+            $primary_photo = $request->file('primary_photo')->store('flights', ["disk" => "public"]);
+            $old = Photo::where('post_id', $flight->id)->where("type" , 'flight')->where("is_main", true)->first();
+            Photo::create([
+                "url" => $primary_photo,
+                "type" => 'flight',
+                "post_id" => $flight->id,
+                "is_main" => true
+            ]);
+            
+            $old->delete();
+        }
+
+        if ($request->hasFile('fligth_photos')) {
+            $request->validate([
+                'fligth_photos.*' => 'mimes:jpeg,png,jpg,gif,svg,webp',
+            ]);
+            $old = Photo::where('post_id', $flight->id)->where("type" , 'flight')->where("is_main", false)->delete();
+            foreach ($request->file('fligth_photos') as $photo) {
+                $fligth_photos = $photo->store('flights', ["disk" => "public"]);
+                Photo::create([
+                    "url" => $fligth_photos,
+                    "type" => 'flight',
+                    "post_id" => $flight->id,
+                    "is_main" => false
+                ]);
+            }
+        }
+        return redirect()->route('flights')->with('success', 'Flight updated successfully');
+    }
+
 
     public function delete(Flight $flight)
     {
