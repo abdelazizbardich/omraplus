bdd7c78175eee106b8498b1713f79cf766069acf
Abdelaziz Bardich <abdelazizbardich@gmail.com>
2025-03-19 19:35:54 +0000

    enable maintenance mode

--- routes/console.php
+++ routes/console.php
@@ -11,3 +11,11 @@ Artisan::command('deploy', function () {
     $output = shell_exec('git reset --hard && git pull && npm run build && php artisan optimize');
     return $this->comment($output);
 })->purpose('Deploy the latest changes from GitHub');
+
+
+Artisan::command('stop', function () {
+    // reset all changes
+    $output = shell_exec('php artisan up');
+    $output = shell_exec('php artisan down --with-secret --secret=pass');
+    $this->comment($output);
+})->purpose('Rollback the latest changes from GitHub');
