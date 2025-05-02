432771b3a1a4ab1bc3a05852729c63d981a684e2
Abdelaziz Bardich <abdelazizbardich@gmail.com>
2025-05-02 23:55:55 +0100

    Update omra-packs.blade.php

--- resources/views/guest/omra-packs.blade.php
+++ resources/views/guest/omra-packs.blade.php
@@ -3,11 +3,13 @@
     <x-page-header :title="__('index.Omra packs')" :breadcrumbs="['Home', 'Omra packs']" />
     <x-spacer></x-spacer>
     <div class="container mx-auto">
-        @foreach ($omraPacks as $pack)
-            <div class="min-w-[350px] xl:w-3/12 lg:w-4/12 md:w-6/12 sm:w-6/12 xs:w-full p-3 pb-6">
-                <x-offer-card :data=$pack></x-offer-card>
-            </div>
-        @endforeach
+        <div class="flex">
+            @foreach ($omraPacks as $pack)
+                <div class="min-w-[350px] xl:w-3/12 lg:w-4/12 md:w-6/12 sm:w-6/12 xs:w-full p-3 pb-6">
+                    <x-offer-card :data=$pack></x-offer-card>
+                </div>
+            @endforeach
+        </div>
     </div>
     @if ($omraPacks->count() > 0)
         <div class="container mx-auto">
