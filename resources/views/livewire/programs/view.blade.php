26efe3070f1a84f30c70d5a349b2fe066b3ab027
Abdelaziz Bardich <abdelazizbardich@gmail.com>
2026-03-23 21:52:33 +0100

    Fix method call for program name rendering in view component

--- resources/views/livewire/programs/view.blade.php
+++ resources/views/livewire/programs/view.blade.php
@@ -6,7 +6,7 @@
             <label for="program-{{ $program->id }}" class="w-full flex flex-col gap-3 justify-center items-center bg-white rounded-lg p-3 peer-checked:bg-gradient-to-br  peer-checked:from-yellow-300  peer-checked:to-yellow-500 cursor-pointer">
                 <img src="{{ Vite::asset('resources/images/programs.png') }}" alt="{{__('index.programs')}}" class="w-8 block">
                 <span class="text-nowrap ">
-                    {{ $program->name }}
+                    {{ $program->name() }}
                 </span>
             </label>
         </div>
